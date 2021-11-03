1030d140068198d145bc90bae35c45a6dfd17844
Anton Cristo <antoncristo92@gmail.com>
2021-11-03 12:59:42 +0200

    static memo card continued

--- client/src/components/router/components/taskin-page/components/inner-router/components/memos/components/memos-items/memos-items.tsx
+++ client/src/components/router/components/taskin-page/components/inner-router/components/memos/components/memos-items/memos-items.tsx
@@ -1,4 +1,8 @@
+import dayjs from "dayjs";
+import { observer } from "mobx-react";
+
 import { Memo } from "../../../../../../../../../../client-types";
+import { memoStore } from "../../../../../../../../../../stores";
 import { MemoCard } from "./components";
 
 import classes from "./memos-items.module.css";
@@ -7,33 +11,77 @@ const memoMocks: Memo[] = [
   {
     content: "Mock memo content, this text is a description of the memo card",
     createdBy: "asd-qwe234-asdassd-1qwerq",
-    creationDate: new Date().getUTCMilliseconds(),
-    dueDate: new Date().getUTCMilliseconds() + 860000,
+    creationDate: dayjs().valueOf(),
+    dueDate: dayjs().add(4, "day").valueOf(),
     title: "Mock Mock",
     uuid: "zxc-yukyuk-2rrr23eds-r32re",
     version: "0.1",
     label: "Urgent",
+    isDone: false,
   },
   {
     content:
       "Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card",
     createdBy: "asd-qwe234-asdassd-jaskldj",
-    creationDate: new Date().getUTCMilliseconds(),
-    dueDate: new Date().getUTCMilliseconds() + 860000,
-    title: "My Memo",
+    creationDate: dayjs().valueOf(),
+    dueDate: dayjs().add(4, "day").valueOf(),
+    title: "My Memo 12",
     uuid: "zqc-yukyuk-2rrr23eds-r32re",
     version: "0.1",
     label: "Ok",
+    isDone: true,
+  },
+  {
+    content:
+      "Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card",
+    createdBy: "asd-qwe234-asdassd-jaskldj",
+    creationDate: dayjs().valueOf(),
+    dueDate: dayjs().add(4, "day").valueOf(),
+    title: "My Memo 25",
+    uuid: "zql-yukyuk-2rrr23eds-r32re",
+    version: "0.1",
+    label: "Ok",
+    isDone: true,
+  },
+  {
+    content:
+      "Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card,Mock memo content, this text is a description of the memo card",
+    createdBy: "asd-qwe234-asdassd-jaskldj",
+    creationDate: dayjs().valueOf(),
+    dueDate: dayjs().add(4, "day").valueOf(),
+    title: "My Memo",
+    uuid: "zqz-yukyuk-2rrr23eds-r32re",
+    version: "0.1",
+    label: "Ok",
+    isDone: false,
   },
 ];
 
-export const MemosItems = () => {
+export const MemosItems = observer(() => {
+  const { uiStoreInstance } = memoStore;
+
+  const memosRenderPipeline = () => {
+    const titleSearchResults = memoMocks.filter((memo) =>
+      memo.title.includes(uiStoreInstance.memoSearchText)
+    );
+    //TODO: after filtering and sorting is implemented
+    //continue the pipeline
+
+    return Array.from(new Set<Memo>([...titleSearchResults]));
+  };
+
+  const renderPipelineResults = memosRenderPipeline();
+
   return (
     <div className={classes.memosItems}>
       {/* //TODO: add mobile memo card design */}
-      {memoMocks.map((memo, index) => (
-        <MemoCard key={memo.uuid} memo={memo} />
-      ))}
+      {renderPipelineResults.length ? (
+        renderPipelineResults.map((memo, index) => (
+          <MemoCard key={memo.uuid} memo={memo} />
+        ))
+      ) : (
+        <div>empty</div>
+      )}
     </div>
   );
-};
+});
