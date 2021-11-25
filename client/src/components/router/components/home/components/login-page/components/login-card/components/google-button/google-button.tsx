4d23577c4983a98ba5d4a915735cd80787db142c
Anton Cristo <antoncristo92@gmail.com>
2021-11-25 09:57:04 +0200

    updated google client id

--- client/src/components/router/components/home/components/login-page/components/login-card/components/google-button/google-button.tsx
+++ client/src/components/router/components/home/components/login-page/components/login-card/components/google-button/google-button.tsx
@@ -5,7 +5,7 @@ import { userStore } from "src/stores";
 import classes from "./google-button.module.css";
 
 const GOOGLE_CLIENT_ID =
-  "344075084830-c3j08bmg8fa0p7acbs8d9i1rnfht40r5.apps.googleusercontent.com";
+  "344075084830-gtrfldn8ilbkt7grgktkd8rr7mtvq13k.apps.googleusercontent.com";
 
 export const GoogleButton = () => {
   const { saveGoogleUserData } = userStore;
