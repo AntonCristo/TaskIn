a135ff75201decddd929742af7cddc28efdbe96a
Anton Cristo <antoncristo92@gmail.com>
2021-12-19 10:17:17 +0200

    cleanup

--- client/src/components/router/components/taskin-page/components/header/header.tsx
+++ client/src/components/router/components/taskin-page/components/header/header.tsx
@@ -1,17 +1,8 @@
-import { memosService } from "src/services";
 import { UserInfo, MemoColorMap, MobileMenuButton } from "./components";
 
 import classes from "./header.module.css";
 
 export const Header = () => {
-  const testPingTemp = () => {
-    memosService.testApiPing();
-  };
-
-  const testInsertMock = () => {
-    memosService.testInsertMockToDB();
-  };
-
   return (
     <div className={classes.header}>
       <UserInfo />
@@ -19,8 +10,6 @@ export const Header = () => {
       <div className={classes.memoColorsMapWrapper}>
         <MemoColorMap />
       </div>
-      <button onClick={testPingTemp}>ping</button>
-      <button onClick={testInsertMock}>insert mock</button>
     </div>
   );
 };
