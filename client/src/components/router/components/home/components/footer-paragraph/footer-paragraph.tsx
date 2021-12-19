3a696fdc0a2fffdbe2dfe947948f8e110ffd05bc
Anton Cristo <antoncristo92@gmail.com>
2021-12-19 11:11:41 +0200

    content update

--- client/src/components/router/components/home/components/footer-paragraph/footer-paragraph.tsx
+++ client/src/components/router/components/home/components/footer-paragraph/footer-paragraph.tsx
@@ -3,10 +3,7 @@ import classes from "./footer-paragraph.module.css";
 export const FooterParagraph = () => {
   return (
     <div className={classes.footerParagraph}>
-      <strong>
-        Like a traffic light, This app will tell you when to hit the breaks and
-        when to keep going {";)"}
-      </strong>
+      <strong>Color your priorities!</strong>
     </div>
   );
 };
